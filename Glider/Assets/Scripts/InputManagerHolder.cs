0d42142ed4444c937099bef2f15a373dc0b44d4c
ZclipseCode <bmichaelbusiness@gmail.com>
2023-02-28 22:30:13 -0600

    input manager holder

---
+++ Glider/Assets/Scripts/InputManagerHolder.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InputManagement.Inputs;
+
+public class InputManagerHolder : MonoBehaviour
+{
+    [SerializeField] InputManager inputManager;
+
+    // ...that's it
+}
