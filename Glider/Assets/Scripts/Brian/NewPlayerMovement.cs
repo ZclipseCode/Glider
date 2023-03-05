11f67690021a07bfc1394efe0fcc1caaadb1ddad
ZclipseCode <bmichaelbusiness@gmail.com>
2023-03-05 02:35:01 -0600

    movement works again (do not ask)

--- Glider/Assets/Scripts/Brian/NewPlayerMovement.cs
+++ Glider/Assets/Scripts/Brian/NewPlayerMovement.cs
@@ -10,23 +10,23 @@ using UnityEngine.EventSystems;
 [RequireComponent(typeof(Rigidbody))]
 public class NewPlayerMovement : MonoBehaviour
 {
+    [Header("Lucas Inputs")]
     [SerializeField] BidirectionalFloat wasd;
-    Rigidbody body;
-
-    // additional movement
     [SerializeField] FloatVariable space;
-    [SerializeField] float moveSpeed, jumpForce, jumpCooldown;
-    bool readyToJump = true;
-    [SerializeField] float playerHeight;
+
+    [Header("Movement")]
+    [SerializeField] float moveSpeed = 3;
+    [SerializeField] float jumpForce = 5;
+    [SerializeField] float jumpCooldown = 1;
+    [SerializeField] float playerHeight = 1.01f;
     [SerializeField] LayerMask whatIsGround;
+    Rigidbody body;
+    bool readyToJump = true;
     bool grounded;
-    [SerializeField] Transform orientation;
-    //bool isJumping;
 
     private void Awake()
     {
         body = GetComponent<Rigidbody>();
-
         body.freezeRotation = true;
     }
 
@@ -34,69 +34,21 @@ public class NewPlayerMovement : MonoBehaviour
     {
         grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight, whatIsGround);
         
-        //MovePlayer();
         float xInput = wasd.Right.Value - wasd.Left.Value;
         float yInput = wasd.Up.Value - wasd.Down.Value;
-        //body.velocity = xInput * transform.right * moveSpeed + yInput * transform.forward * moveSpeed;
-        body.velocity = new Vector3(xInput * moveSpeed, body.velocity.y, yInput * moveSpeed);
-
-        //SpeedControl();
-
-        //body.drag = groundDrag;
+        body.velocity = xInput * transform.right * moveSpeed + yInput * transform.forward * moveSpeed + transform.up * body.velocity.y;
 
         float jumpInput = space.Value;
         if (jumpInput > 0 && readyToJump && grounded)
         {
             readyToJump = false;
-            //isJumping = true;
             body.velocity += Vector3.up * jumpForce;
             Invoke(nameof(ResetJump), jumpCooldown);
         }
-        //if (isJumping)
-        //{
-        //    //body.velocity = xInput * transform.right * moveSpeed + yInput * transform.forward * moveSpeed + transform.up * jumpForce;
-        //    //body.velocity = new Vector3(body.velocity.x, body.velocity.y + jumpForce, body.velocity.z);
-        //    body.velocity += Vector3.up * jumpForce;
-        //    Invoke(nameof(ResetJump), jumpCooldown);
-        //}
-        //if (jumpInput > 0 && readyToJump && grounded)
-        //{
-        //    readyToJump = false;
-        //    Jump();
-        //    Invoke(nameof(ResetJump), jumpCooldown);
-        //}
-    }
-
-    void MovePlayer()
-    {
-        float xInput = wasd.Right.Value - wasd.Left.Value;
-        float yInput = wasd.Up.Value - wasd.Down.Value;
-        //Vector3 moveDirection = orientation.forward * yInput + orientation.right * xInput;
-        //body.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
-        body.velocity = xInput * transform.right * moveSpeed + yInput * transform.forward * moveSpeed + Vector3.up * 0;
-    }
-
-    //void SpeedControl()
-    //{
-    //    Vector3 flatVel = new Vector3(body.velocity.x, 0f, body.velocity.z);
-
-    //    //if (flatVel.magnitude > moveSpeed)
-    //    //{
-    //    //    Vector3 limitedVel = flatVel.normalized * moveSpeed;
-    //    //    body.velocity = new Vector3(limitedVel.x, body.velocity.y, limitedVel.z);
-    //    //}
-    //}
-
-    void Jump()
-    {
-        //body.velocity = new Vector3(body.velocity.x, 0f, body.velocity.z);
-        body.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-        //transform.up vs Vector3.up: transform is relative to GameObject's transform while Vector3 is world space
     }
 
     void ResetJump()
     {
         readyToJump = true;
-        //isJumping = false;
     }
 }
