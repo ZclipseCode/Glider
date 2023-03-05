3c03354f9870dbc0edf19f7c584aaf3efdff335b
Atlas1004 <Schneideea04@uww.edu>
2023-03-05 15:26:18 -0600

    WIP Gliding

---
+++ Glider/Assets/Scripts/Ethan/EthanNewPlayerMovement.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using InputManagement;
+using UnityEngine.Serialization;
+using ReferenceVariables;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Rigidbody))]
+public class EthanNewPlayerMovement : MonoBehaviour
+{
+    [Header("Lucas Inputs")]
+    [SerializeField] BidirectionalFloat wasd;
+    [SerializeField] FloatVariable space;
+
+    [Header("Movement")]
+    [SerializeField] float moveSpeed = 3;
+    [SerializeField] float jumpForce = 5;
+    [SerializeField] float jumpCooldown = 1;
+    [SerializeField] float playerHeight = 1.01f;
+    [SerializeField] LayerMask whatIsGround;
+    Rigidbody body;
+    bool readyToJump = true;
+    bool grounded;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+        body.freezeRotation = true;
+    }
+
+    private void FixedUpdate()
+    {
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight, whatIsGround);
+        
+        float xInput = wasd.Right.Value - wasd.Left.Value;
+        float yInput = wasd.Up.Value - wasd.Down.Value;
+        body.velocity = xInput * transform.right * moveSpeed + yInput * transform.forward * moveSpeed + transform.up * body.velocity.y;
+
+        float jumpInput = space.Value;
+        if (jumpInput > 0 && readyToJump && grounded)
+        {
+            readyToJump = false;
+            body.velocity += Vector3.up * jumpForce;
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+    }
+
+    void ResetJump()
+    {
+        readyToJump = true;
+    }
+    public bool GetReadyToJump() {
+        return readyToJump;
+    }
+}
