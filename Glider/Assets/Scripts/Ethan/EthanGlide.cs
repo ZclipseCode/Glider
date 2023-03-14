d7386b457e44d2e4873381708f677b13c1a774fa
Lucas Ackman <AckmanLD@gmail.com>
2023-03-14 18:16:47 -0500

    Improved Momentum System

--- Glider/Assets/Scripts/Ethan/EthanGlide.cs
+++ Glider/Assets/Scripts/Ethan/EthanGlide.cs
@@ -1,11 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using System;
-using InputManagement;
-using UnityEngine.Serialization;
 using ReferenceVariables;
-using UnityEngine.EventSystems;
 
 public class EthanGlide : MonoBehaviour
 {
@@ -14,33 +8,63 @@ public class EthanGlide : MonoBehaviour
     [SerializeField] Camera cam;
     [SerializeField] FloatVariable space;
     [SerializeField] LayerMask whatIsGround;
-    [SerializeField] float playerHeight = 1.01f, momentum;
-    [SerializeField] bool grounded, gliding = false;
-    float terminalVelocity = 60;
-    private void Start() {
+    [SerializeField] float playerHeight = 1.01f;
+    [SerializeField] float gainAmount;
+    [SerializeField] float loseAmount;
+    [SerializeField] float angle;
+    [SerializeField] float terminalVelocity = 60;
+    float momentum;
+    bool grounded, gliding = false;
+
+    private void Start()
+    {
         body = GetComponent<Rigidbody>();
     }
-    private void FixedUpdate() {
+
+    private void FixedUpdate()
+    {
         float jumpInput = space.Value;
         grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight, whatIsGround);
 
-        if(movement.GetReadyToJump() && jumpInput > 0 && !grounded) {
+        if (movement.GetReadyToJump() && jumpInput > 0 && !grounded)
+        {
             gliding = true;
             momentum = 2;
         }
-        if (grounded || momentum < 1) {
+        if (grounded)
+        {
             gliding = false;
             momentum = 0;
         }
 
-        if (gliding) {
-            if (cam.transform.rotation.x > 0) {
-                    momentum = Mathf.Lerp(momentum, terminalVelocity, cam.transform.rotation.x / 10);
+        if (gliding)
+        {
+            Vector3 rotation = cam.transform.rotation.eulerAngles;
+            if (rotation.x < angle)
+            {
+                float change = Mathf.Lerp(0, gainAmount, rotation.x / 90);
+                momentum += change;
             }
-            else if (cam.transform.rotation.x < 0) { 
-                    momentum = Mathf.Lerp(momentum, 0, Mathf.Abs(cam.transform.rotation.x / 10));
+            else if (rotation.x > angle)
+            {
+                rotation.x = 360 - rotation.x;
+                float change = Mathf.Lerp(0, loseAmount, Mathf.Abs(rotation.x / 90));
+                momentum -= change;
             }
-            body.velocity = cam.transform.right * 0 + momentum * cam.transform.forward + cam.transform.up * 0;
+            momentum = Mathf.Clamp(momentum, 0, terminalVelocity);
+            bool glide = false;
+            if (body.velocity.magnitude < 1)
+            {
+                return;
+            }
+            if (momentum > 1)
+            {
+                glide = true;
+            }
+            if (glide)
+            {
+                body.velocity = momentum * cam.transform.forward;
+            }
         }
     }
 }
